39acc6c4b1dee47bd67ebf6a6ca5658720670218
Ngô Lê Tuyết Hoa <80042406+tuyethoa1011@users.noreply.github.com>
2023-04-22 19:04:57 +0700

    Upload code
    - draw pixel
    - complete init ssd1306
    - drawbitmap
    - Add dependencies software folder
    Plans: Might clean code later

---
+++ ssd1306.c
@@ -0,0 +1,215 @@
+#include "ssd1306.h"
+#include "font8x8_basic.h"
+
+/* SSD1306 data buffer */
+static uint8_t SSD1306_Buffer[SSD1306_BUFFER_SIZE];
+
+
+
+/*Private Variable*/
+static SSD1306_t SSD1306;
+
+void ssd1306_init(i2c_port_t i2c_num)
+{
+    static const char *TAG = "i2c-lib";
+	esp_err_t espRc;
+
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (SSD1306_OLED_ADDR << 1) | I2C_MASTER_WRITE, SSD1306_ACK);
+	i2c_master_write_byte(cmd, SSD1306_CONTROL_BYTE_CMD_STREAM , SSD1306_ACK);
+
+    i2c_master_write_byte(cmd, SSD1306_DISPLAY_OFF, SSD1306_ACK);
+    i2c_master_write_byte(cmd,SSD1306_MEMORY_ADDR_MODE, SSD1306_ACK);
+    i2c_master_write_byte(cmd,SSD1306_SET_MUX_RATIO,SSD1306_ACK); //set mux ratio
+    i2c_master_write_byte(cmd,SSD1306_DISPLAY_OFFSET, SSD1306_ACK); //set offset
+    i2c_master_write_byte(cmd,SSD1306_SET_START_LINE, SSD1306_ACK);
+
+    i2c_master_write_byte(cmd, SSD1306_SEG_REMAP_OP, SSD1306_ACK); // reverse left-right mapping
+	i2c_master_write_byte(cmd, SSD1306_COM_SCAN_DIR_OP, SSD1306_ACK); // reverse up-bottom mapping
+
+    i2c_master_write_byte(cmd,SSD1306_COM_PIN_CONF,SSD1306_ACK);
+    i2c_master_write_byte(cmd,SSD1306_SET_CONTRAST,SSD1306_ACK); //set contrast
+
+    i2c_master_write_byte(cmd,SSD1306_DISPLAY_ENT_DISP_ON,SSD1306_ACK);
+    
+	i2c_master_write_byte(cmd, SSD1306_SET_CHARGE_PUMP , SSD1306_ACK);
+	i2c_master_write_byte(cmd, SSD1306_DISPLAY_NORMAL, SSD1306_ACK);
+    i2c_master_write_byte(cmd,SSD1306_SET_OSC_FREQ,SSD1306_ACK);
+
+    i2c_master_write_byte(cmd, 0x14, SSD1306_ACK); //enable charge pump 
+
+	i2c_master_write_byte(cmd,SSD1306_DISPLAY_ON, SSD1306_ACK); //turn on light panel
+	i2c_master_write_byte(cmd,SSD1306_DEACT_SCROLL, SSD1306_ACK); //deactivate scroll
+
+	i2c_master_stop(cmd);
+
+	espRc = i2c_master_cmd_begin(i2c_num, cmd, 10/portTICK_PERIOD_MS); //config thời gian delay thành 1s
+	if (espRc == ESP_OK) {
+		SSD1306.Initialized = 1;
+		ESP_LOGI(TAG, "OLED configured successfully");
+	} else {
+		ESP_LOGE(TAG, "OLED configuration failed. code: 0x%.2X", espRc);
+	}
+	i2c_cmd_link_delete(cmd);
+
+	/*Clear screen*/
+	ssd1306_Fill(SSD1306_COLOR_BLACK);
+
+	/*Update Screen*/
+	ssd1306_UpdateScreen(i2c_num);
+
+	/*Set default Value*/
+	SSD1306.Current_X = 0;
+	SSD1306.Current_Y = 0;
+
+}
+//ssd1306_init done
+
+void ssd1306_clear(i2c_port_t i2c_num)
+{
+	ssd1306_Fill(0);
+	ssd1306_UpdateScreen(i2c_num);	
+}
+
+void ssd1306_Fill(SSD1306_COLOR_t color) 
+{
+	/* Set memory */
+	memset(SSD1306_Buffer, (color == SSD1306_COLOR_BLACK) ? 0x00 : 0xFF, sizeof(SSD1306_Buffer));
+}
+
+void ssd1306_string_text(const void *arg_text, i2c_port_t i2c_num)
+{
+	char *text = (char*)arg_text;
+	uint8_t text_len = strlen(text);
+
+	i2c_cmd_handle_t cmd;
+
+	uint8_t cur_page = 0;
+
+	cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (SSD1306_OLED_ADDR<< 1) | I2C_MASTER_WRITE, true);
+
+	i2c_master_write_byte(cmd, SSD1306_CONTROL_BYTE_CMD_STREAM, true);
+	i2c_master_write_byte(cmd, 0x00, true); // reset column - choose column --> 0
+	i2c_master_write_byte(cmd,0x10, true); // reset line - choose line --> 0
+
+	i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset page
+	//0xB0 = 1011 0000 
+	i2c_master_stop(cmd);
+	i2c_master_cmd_begin(i2c_num, cmd, 10/portTICK_PERIOD_MS);
+	i2c_cmd_link_delete(cmd);
+
+	for (uint8_t i = 0; i < text_len; i++) {
+		if (text[i] == '\n') {
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (SSD1306_OLED_ADDR << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, SSD1306_CONTROL_BYTE_CMD_STREAM, true);
+			i2c_master_write_byte(cmd, 0x00, true); // reset column
+			i2c_master_write_byte(cmd, 0x10, true);
+			i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); // increment page
+
+			i2c_master_stop(cmd);
+			i2c_master_cmd_begin(i2c_num, cmd, 10/portTICK_PERIOD_MS);
+			i2c_cmd_link_delete(cmd);
+		} else {
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (SSD1306_OLED_ADDR << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd,SSD1306_CONTROL_BYTE_DATA_STREAM , true);
+			i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
+
+			i2c_master_stop(cmd);
+			i2c_master_cmd_begin(i2c_num, cmd, 10/portTICK_PERIOD_MS);
+			i2c_cmd_link_delete(cmd);
+		}
+	}
+}
+
+void ssd1306_setCursor(uint16_t x, uint16_t y)
+{
+	SSD1306.Current_X = x;
+	SSD1306.Current_Y = y;
+}
+
+void ssd1306_DrawPixel(uint16_t x, uint16_t y, SSD1306_COLOR_t color)
+{
+	if(x >= SSD1306_WIDTH || y >= SSD1306_HEIGHT)
+	{
+		/*Throw ERROR*/
+		return;
+	}
+
+	
+	/*Check if pixel is inverted*/
+	/*Kiểm tra xem có pixel nào bị ngược không, nếu có thì điều chỉnh lại*/
+	/*if (SSD1306.Inverted) {
+		color = (SSD1306_COLOR_t)!color;
+	}
+	*/
+
+	/* Set color */
+	/*Chủ yếu là để set đúng màu pixel được vẽ ra*/
+	if (color == SSD1306_COLOR_WHITE) {
+		//set phần tử in white for sure
+		SSD1306_Buffer[x + (y / 8) * SSD1306_WIDTH] |= 1 << (y % 8); //oke understood, CHƯA HIỂU ỦA CÁI INDEX LỰA CHỌN NÓ TÍNH NHƯ NÀO MÀ HAY QUÁ NHỜ
+	} else { //set phần tử in black for sure 
+		SSD1306_Buffer[x + (y / 8) * SSD1306_WIDTH] &= ~(1 << (y % 8));
+	}	
+}
+
+
+//Software bitmap converter recommend: https://sourceforge.net/projects/lcd-image-converter/
+void ssd1306_drawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, uint16_t color)
+{
+	int16_t byteWidth = (w + 7) / 8; // Bitmap scanline pad = whole byte
+    uint8_t byte = 0;
+
+    if (x >= SSD1306_WIDTH || y >= SSD1306_HEIGHT) {
+        return;
+    }
+
+    for (uint8_t j = 0; j < h; j++, y++) {
+        for (uint8_t i = 0; i < w; i++) {
+            if (i & 7) {
+                byte <<= 1;
+            } else {
+                byte = (*(const unsigned char *)(&bitmap[j * byteWidth + i / 8]));
+            }
+
+            if (byte & 0x80) {
+                ssd1306_DrawPixel(x+i,y, color);
+            }
+        }
+    }
+    return;
+}
+
+void ssd1306_UpdateScreen(i2c_port_t i2c_num)
+{
+	uint8_t page;
+	i2c_cmd_handle_t cmd;
+
+	for(page = 0; page < SSD1306_HEIGHT/8; page++)
+	{
+		cmd = i2c_cmd_link_create();
+		i2c_master_start(cmd);
+		i2c_master_write_byte(cmd, (SSD1306_OLED_ADDR << 1) | I2C_MASTER_WRITE, true);
+		i2c_master_write_byte(cmd, SSD1306_CONTROL_BYTE_CMD_SINGLE , true);
+		
+		i2c_master_write_byte(cmd, 0xB0 | page, true);
+
+		i2c_master_write_byte(cmd, SSD1306_CONTROL_BYTE_DATA_STREAM, true);
+		i2c_master_write(cmd, &SSD1306_Buffer[SSD1306_WIDTH*page],SSD1306_WIDTH, true);
+		i2c_master_stop(cmd);
+		i2c_master_cmd_begin(i2c_num, cmd, 10/portTICK_PERIOD_MS);
+		i2c_cmd_link_delete(cmd);
+	}
+	
+}
+
---
+++ ssd1306.h
@@ -0,0 +1,123 @@
+/****************************************************************************************************************
+   .Title SSD1306 OLED Driver
+  ***************************************************************************************************************
+  File:	      ssd1306.h
+  Modifier:   Ngo Le Tuyet Hoa
+  Last Modified:    14th APR 2023
+  ***************************************************************************************************************
+  Copyright (C) 2023 https://github.com/tuyethoa1011
+  This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
+  of the GNU General Public License version 3 as published by the Free Software Foundation.
+  This software library is shared with public for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
+  or indirectly by this software, read more about this on the GNU General Public License.
+  ***************************************************************************************************************
+    .Usage: Basic setup for uses OLED Display
+*/  
+
+#ifndef SSD1306_H
+#define SSD1306_H
+
+//Includes
+#include <string.h>
+#include <stdlib.h>
+#include "driver/i2c.h"
+#include "esp_log.h"
+
+
+//Define SSD1306 width and length
+#define SSD1306_WIDTH 128
+#define SSD1306_HEIGHT 64
+
+//Success/Errors
+#define SSD1306_OK 1
+#define SSD1306_FAIL 0
+
+//ACK/NACK
+#define SSD1306_ACK 1
+#define SSD1306_NACK
+
+//OLED SSD1306 Address definition
+#define SSD1306_OLED_ADDR 0x3C
+
+//Command definition
+//-------------------- Control byte ---------------------------
+  #define SSD1306_CONTROL_BYTE_CMD_SINGLE    0x80 //Control byte dùng để thông báo các byte I2C OLED nhận tiếp theo nhận duy nhất 1 byte lệnh
+  #define SSD1306_CONTROL_BYTE_CMD_STREAM    0x00 //Control byte dùng để thông báo các byte I2C OLED nhận tiếp theo là 1 luồng lệnh nhiều bytes
+  #define SSD1306_CONTROL_BYTE_DATA_STREAM   0x40 //Control byte dùng để thông báo các byte I2C OLED nhập tiếp theo là 1 luồng stream toàn các byte dữ liệu (DATA STREAM)
+  #define SSD1306_CONTROL_BYTE_DATA_SINGLE   0xC0 //Continuation bit = 1, D/C = 1100 0000
+//-------------------------------------------------------------
+  #define SSD1306_SET_MUX_RATIO     0xA8
+  #define SSD1306_DISPLAY_OFFSET    0xD3
+  #define SSD1306_DISPLAY_ON        0xAF
+  #define SSD1306_DISPLAY_OFF       0xAE
+  #define SSD1306_DISPLAY_ENT_DISP_ON   0xA4
+  #define SSD1306_DISPLAY_IGNORE_RAM    0xA5
+  #define SSD1306_DISPLAY_NORMAL        0xA6
+  #define SSD1306_DISPLAY_INVERSE       0xA7
+  #define SSD1306_DEACT_SCROLL      0x2E
+  #define SSD1306_ACTIVE_SCROLL     0x2F
+  #define SSD1306_SET_START_LINE    0x40
+  #define SSD1306_MEMORY_ADDR_MODE  0x20
+  #define SSD1306_SET_COLUMN_ADDR   0x21
+  #define SSD1306_SET_PAGE_ADDR     0x22
+  #define SSD1306_SEG_REMAP         0xA0
+  #define SSD1306_SEG_REMAP_OP      0xA1
+  #define SSD1306_COM_SCAN_DIR      0xC0
+  #define SSD1306_COM_SCAN_DIR_OP   0xC8
+  #define SSD1306_COM_PIN_CONF      0xDA
+  #define SSD1306_SET_CONTRAST      0x81
+  #define SSD1306_SET_OSC_FREQ      0xD5
+  #define SSD1306_SET_CHARGE_PUMP     0x8D
+  #define SSD1306_SET_PRECHARGE     0xD9
+  #define SSD1306_VCOM_DESELECT     0xDB
+
+	// AREA definition
+  // ------------------------------------------------------------------------------------
+  #define START_PAGE_ADDR           0
+  #define END_PAGE_ADDR             7     // 7 for 128x64, 3 for 128x32 version
+  #define START_COLUMN_ADDR         0
+  #define END_COLUMN_ADDR           127
+  #define RAM_X_END                 END_COLUMN_ADDR + 1
+  #define RAM_Y_END                 END_PAGE_ADDR + 1
+
+  #define CACHE_SIZE_MEM            (1 + END_PAGE_ADDR) * (1 + END_COLUMN_ADDR)
+
+  #define MAX_X                     END_COLUMN_ADDR
+  #define MAX_Y                     (END_PAGE_ADDR + 1) * 8
+
+
+//Define Color Fill
+#define SSD1306_COLOR_BLACK 0x00 //black color
+#define SSD1306_COLOR_WHITE 0x01 //white color
+
+//define ssd1306 buffer
+#define SSD1306_BUFFER_SIZE SSD1306_WIDTH * SSD1306_HEIGHT / 8
+typedef enum {
+    Black = 0x00, // Black color, no pixel
+    White = 0x01  // Pixel is set, white color
+} SSD1306_COLOR_t;
+
+/*Private SSD1306 Structure*/
+typedef struct {
+	uint16_t Current_X;
+	uint16_t Current_Y;
+	uint8_t Inverted;
+	uint8_t Initialized;
+} SSD1306_t;
+
+
+//Function prototype definition
+void ssd1306_init(i2c_port_t i2c_num);
+void ssd1306_clear(i2c_port_t i2c_num);
+uint8_t ssd1306_normal_screen(i2c_port_t i2c_num);
+uint8_t ssd1306_invert_screen(i2c_port_t i2c_num);
+void ssd1306_string_text(const void *arg_text, i2c_port_t i2c_num);
+void ssd1306_DrawPixel(uint16_t x, uint16_t y, SSD1306_COLOR_t color);
+void ssd1306_drawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, uint16_t color);
+uint8_t ssd1306_setcontrast(i2c_port_t i2c_num);
+void ssd1306_UpdateScreen(i2c_port_t i2c_num);
+void ssd1306_setCursor(uint16_t x, uint16_t y); //set x,y coordinate
+void ssd1306_Fill(SSD1306_COLOR_t color);
+
+
+#endif                                      
